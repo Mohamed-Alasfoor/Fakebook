71743e1818c19f7680283c57b6bc2c17b38f94a0
Mohamed-Alasfoor <158215288+Mohamed-Alasfoor@users.noreply.github.com>
2025-01-22 15:42:22 +0300

    applied migration version1

--- backend/pkg/db/migrations/000001_create_users_table.up.sql
+++ backend/pkg/db/migrations/000001_create_users_table.up.sql
@@ -1,1 +1,12 @@
-DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id TEXT PRIMARY KEY, -- UUID
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    date_of_birth TEXT NOT NULL,
+    avatar TEXT,
+    nickname TEXT,
+    about_me TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
