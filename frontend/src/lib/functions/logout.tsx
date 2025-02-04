f69dc41f6040465486862fb1e9254097d366238e
Ali <AliHasanJMM@gmail.com>
2025-02-04 20:19:07 +0300

    Fix Logout

--- frontend/src/lib/functions/logout.tsx
+++ frontend/src/lib/functions/logout.tsx
@@ -1,11 +1,17 @@
 import axios from "axios";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-export const handleLogout = async (router:AppRouterInstance)=> {
-    try {
-        await axios.post('http://localhost:8080/logout');
-        router.push('/login');
-    } catch (error) {
-        console.error('Logout failed:', error);
-    }
+export const handleLogout = async (router: AppRouterInstance) => {
+  try {
+    await axios.post(
+      "http://localhost:8080/logout",
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+    router.push("/login");
+  } catch (error) {
+    console.error("Logout failed:", error);
+  }
 };
